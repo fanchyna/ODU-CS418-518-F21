b5f3e0455b7dcf7d7fa06fd0661505e944c41989
Yang(Lily) <erouirior@gmail.com>
2021-11-10 00:09:22 -0500

    projectb: milestone2

---
+++ user_article/app/Console/Commands/LoadArticlesData.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+use App\Article;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class LoadArticlesData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'articles:load';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'load article data from json file';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        //$json = json_decode(Storage::disk('local')::get('articles.json'), true);
+        $path = storage_path( ) ."/articles.json";
+        $file = trim(file_get_contents($path), "\xEF\xBB\xBF");
+        $json = json_decode($file, true);
+        $this->info('Loading articles from json. This might take a while...');
+        
+        foreach ($json as $item) {
+            $this->info("\n write: ");
+            $articleData = [
+                'title' => $item['title'],
+                'author' => $item['author'],
+                'time' => $item['time'],
+                'claim' => $item['claim'],
+            ];
+            $this->info("\n read: ");
+            $this->output->write('.');
+            Article::create($articleData);
+            $this->info("\n create: ");
+            $this->output->write('.');
+            $this->info("\n done: ");
+        }
+        
+        $this->info("\n loading complete");
+    }
+}
---
+++ user_article/database/migrations/2021_11_08_190703_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->text('author');
+            $table->text('time');
+            $table->text('claim');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
